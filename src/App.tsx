d1eeafa30af0d4ebe254b5fb23d5a33dfd6e8a8b
andrei.lobanovich <andrei.lobanovich@akvelon.com>
2024-01-23 00:08:29 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ThemeProvider} from '@mui/material/styles';
+import theme from './elements/Theme';
+import './assets/fonts.css';
+import 'normalize.css';
+import MainPage from "./mainPage/MainPage";
+import React from "react";
+import {Route, Router} from "react-router-dom";
+import {createBrowserHistory} from "history";
+import ApproachPage from "./approachPage/ApproachPage";
+import ProgramPage from "./programPage/ProgramPage";
+import PricingPage from "./pricingPage/PricingPage";
+import EnrollPage from "./enrollPage/EnrollPage";
+
+const history = createBrowserHistory();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Router history={history}>
+                <Route exact path="/" component={MainPage}/>
+                <Route path="/approach" component={ApproachPage}/>
+                <Route path="/our-program" component={ProgramPage}/>
+                <Route path="/pricing" component={PricingPage}/>
+                <Route path="/enroll" component={EnrollPage}/>
+            </Router>
+        </ThemeProvider>
+    );
 }
 
 export default App;
